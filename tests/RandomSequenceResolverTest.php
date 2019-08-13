ed2d36db267ff3c1bf19af2807bac8fae0c7d53d
godruoyi <g@godruoyi.com>
2019-08-13 03:50:21 +0000

    Apply fixes from StyleCI

--- tests/RandomSequenceResolverTest.php
+++ tests/RandomSequenceResolverTest.php
@@ -2,21 +2,20 @@
 
 namespace Tests;
 
-use Godruoyi\Snowflake\Snowflake;
 use Godruoyi\Snowflake\RandomSequenceResolver;
 
 class RandomSequenceResolverTest extends TestCase
 {
     public function testBasic()
     {
-        $random = new RandomSequenceResolver;
+        $random = new RandomSequenceResolver();
 
-        $this->assertTrue($random->sequence(1) === 0);
-        $this->assertTrue($random->sequence(1) === 1);
-        $this->assertTrue($random->sequence(2) === 0);
-        $this->assertTrue($random->sequence(3) === 0);
-        $this->assertTrue($random->sequence(3) === 1);
-        $this->assertTrue($random->sequence(4) === 0);
-        $this->assertTrue($random->sequence(4) === 1);
+        $this->assertTrue(0 === $random->sequence(1));
+        $this->assertTrue(1 === $random->sequence(1));
+        $this->assertTrue(0 === $random->sequence(2));
+        $this->assertTrue(0 === $random->sequence(3));
+        $this->assertTrue(1 === $random->sequence(3));
+        $this->assertTrue(0 === $random->sequence(4));
+        $this->assertTrue(1 === $random->sequence(4));
     }
 }

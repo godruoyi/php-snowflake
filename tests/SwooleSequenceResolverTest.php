86bfab85a96d30313fcd2936210552e121a835d2
Godruoyi <g@godruoyi.com>
2022-04-09 22:41:35 +0800

    👽 Refine test cases

--- tests/SwooleSequenceResolverTest.php
+++ tests/SwooleSequenceResolverTest.php
@@ -31,8 +31,8 @@ class SwooleSequenceResolverTest extends TestCase
         $this->assertTrue(1 == $snowflake->sequence(1));
         $this->assertTrue(2 == $snowflake->sequence(1));
     }
-
     public function testResetLock() {
+
         $snowflake = new SwooleSequenceResolver();
 
         $lock = $this->createStub(\Swoole\Lock::class);
@@ -43,6 +43,9 @@ class SwooleSequenceResolverTest extends TestCase
 
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Swoole lock failure, Unable to get the program lock after many attempts.');
-        $snowflake->sequence(1);
+
+        while (true) {
+            $snowflake->sequence(1);
+        }
     }
 }

14d324e01f86f813b300ea6c95ff136318c4cea7
xavier <hafung@foxmail.com>
2022-07-06 11:06:05 +0800

    feat: add second-level timestamp bit

---
+++ src/IceFlake.php
@@ -0,0 +1,140 @@
+<?php
+
+/*
+ * This file is part of the godruoyi/php-snowflake.
+ *
+ * (c) Godruoyi <g@godruoyi.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Godruoyi\Snowflake;
+
+class IceFlake extends Snowflake
+{
+    const MAX_TIMESTAMP_LENGTH = 32;
+
+    const MAX_MACHINEID_LENGTH = 9;
+
+    const MAX_SEQUENCE_LENGTH = 12;
+
+    /**
+     * The machine ID.
+     *
+     * @var int
+     */
+    protected $machineid;
+
+    /**
+     * Build Sonyflake Instance.
+     *
+     * @param int $machineid machine ID 0 ~ 511 (2^9)-1
+     */
+    public function __construct(int $machineid = 0)
+    {
+        $maxMachineID = -1 ^ (-1 << self::MAX_MACHINEID_LENGTH);
+
+        $this->machineid = $machineid;
+        if ($this->machineid < 0 || $this->machineid > $maxMachineID) {
+            throw new \InvalidArgumentException("Invalid machine ID, must be between 0 ~ {$maxMachineID}.");
+        }
+    }
+
+    /**
+     * Get Sonyflake id.
+     *
+     * @return string
+     */
+    public function id()
+    {
+        $elapsedTime = $this->elapsedTime();
+
+        while (($sequence = $this->callResolver($elapsedTime)) > (-1 ^ (-1 << self::MAX_SEQUENCE_LENGTH))) {
+            $elapsedTime2 = $this->elapsedTime();
+            // Get next timestamp
+            while ($elapsedTime2 == $elapsedTime) {
+                usleep(1);
+                $elapsedTime2 = $this->elapsedTime();
+            }
+            $elapsedTime = $elapsedTime2;
+        }
+
+        $machineidLeftMoveLength = self::MAX_SEQUENCE_LENGTH;
+        $timestampLeftMoveLength = self::MAX_MACHINEID_LENGTH + $machineidLeftMoveLength;
+
+        if ($elapsedTime > (-1 ^ (-1 << self::MAX_TIMESTAMP_LENGTH))) {
+            // The lifetime (174 years).
+            throw new \Exception('Exceeding the maximum life cycle of the algorithm.');
+        }
+
+        return (string) ($elapsedTime << $timestampLeftMoveLength
+            | ($this->machineid << $machineidLeftMoveLength)
+            | ($sequence));
+    }
+
+    /**
+     * Set start time.
+     */
+    public function setStartTimeStamp(int $startTime)
+    {
+        $elapsedTime = time() - $startTime;
+        if ($elapsedTime < 0) {
+            throw new \Exception('The start time cannot be greater than the current time');
+        }
+
+        $maxTimeDiff = -1 ^ (-1 << self::MAX_TIMESTAMP_LENGTH);
+        if ($elapsedTime > $maxTimeDiff) {
+            throw new \Exception('Exceeding the maximum life cycle of the algorithm');
+        }
+
+        $this->startTime = $startTime;
+
+        return $this;
+    }
+
+    /**
+     * Parse snowflake id.
+     */
+    public function parseId(string $id, $transform = false): array
+    {
+        $id = decbin($id);
+        $length = self::MAX_SEQUENCE_LENGTH + self::MAX_MACHINEID_LENGTH;
+
+        $data = [
+            'sequence' => substr($id, -1 * self::MAX_SEQUENCE_LENGTH),
+            'machineid' => substr($id, -1 * $length, self::MAX_MACHINEID_LENGTH),
+            'timestamp' => substr($id, 0, $length),
+        ];
+
+        return $transform ? array_map(function ($value) {
+            return bindec($value);
+        }, $data) : $data;
+    }
+
+    /**
+     * Get start timestamp, If not set default to 2022-02-06 17:55:08.
+     *
+     * @return int
+     */
+    public function getStartTimeStamp()
+    {
+        if ($this->startTime > 0) {
+            return $this->startTime;
+        }
+
+        // We set a default start time if you not set.
+        $defaultTime = '2022-02-06 17:55:08';
+
+        return strtotime($defaultTime);
+    }
+
+    /**
+     * The Elapsed Time.
+     *
+     * @return int
+     */
+    private function elapsedTime()
+    {
+        return time() - $this->getStartTimeStamp();
+    }
+}
---
+++ tests/IceFlakeTest.php
@@ -0,0 +1,152 @@
+<?php
+
+/*
+ * This file is part of the godruoyi/php-snowflake.
+ *
+ * (c) Godruoyi <g@godruoyi.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Tests;
+
+use Godruoyi\Snowflake\IceFlake;
+use Godruoyi\Snowflake\RandomSequenceResolver;
+use Godruoyi\Snowflake\SequenceResolver;
+
+class IceFlakeTest extends TestCase
+{
+    public function testBasic()
+    {
+        $snowflake = new IceFlake();
+        $this->assertInstanceOf(IceFlake::class, $snowflake);
+
+        $snowflake = new IceFlake(0);
+        $this->assertInstanceOf(IceFlake::class, $snowflake);
+        $this->assertEquals(0, $this->invokeProperty($snowflake, "machineid"));
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid machine ID, must be between 0 ~ 511.');
+        $snowflake = new IceFlake(-1);
+
+        $snowflake = new IceFlake(511);
+        $this->assertInstanceOf(IceFlake::class, $snowflake);
+        $this->assertEquals(511, $this->invokeProperty($snowflake, "machineid"));
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid machine ID, must be between 0 ~ 511.');
+        $snowflake = new IceFlake(512);
+    }
+
+    public function testSetStartTimeStamp()
+    {
+        $snowflake = new IceFlake(110);
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Exceeding the maximum life cycle of the algorithm');
+        $snowflake->setStartTimeStamp(strtotime('1840-01-01 00:00:00')); // 2021 - 1840 = 181 > The lifetime (174 years)
+    }
+
+    public function testSetStartTimeStampCannotGreaterThanCurrentTime () {
+        $snowflake = new IceFlake(110);
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('The start time cannot be greater than the current time');
+        $snowflake->setStartTimeStamp(strtotime('2345-01-01 00:00:00'));
+
+        $snowflake->setStartTimeStamp(1);
+        $this->assertEquals(1, $snowflake->getStartTimeStamp());
+    }
+
+    public function testSetStartTimeStampBasic () {
+        $snowflake = new IceFlake(110);
+
+        $snowflake->setStartTimeStamp(1);
+        $this->assertEquals(1, $snowflake->getStartTimeStamp());
+    }
+
+    public function testParseId()
+    {
+        $snowflake = new IceFlake(110);
+        $id = $snowflake->id();
+
+        $dumps = $snowflake->parseId($id);
+        $this->assertArrayHasKey('sequence', $dumps);
+        $this->assertArrayHasKey('machineid', $dumps);
+        $this->assertArrayHasKey('timestamp', $dumps);
+        $this->assertTrue(decbin(110) == $dumps['machineid']);
+
+        $dumps = $snowflake->parseId($id, true);
+        $this->assertArrayHasKey('sequence', $dumps);
+        $this->assertArrayHasKey('machineid', $dumps);
+        $this->assertArrayHasKey('timestamp', $dumps);
+        $this->assertTrue(110 == $dumps['machineid']);
+    }
+
+    public function testId()
+    {
+        $snowflake = new IceFlake();
+        $id = $snowflake->id();
+        $this->assertTrue(!empty($id));
+
+        $datas = [];
+        for ($i = 0; $i < 100; ++$i) {
+            $id = $snowflake->id();
+            // $this->assertArrayNotHasKey($id, $datas);
+            $datas[$id] = 1;
+        }
+        $this->assertTrue(100 === count($datas));
+    }
+
+    public function testGenerateID() {
+        $snowflake = new IceFlake(1);
+        $snowflake->setStartTimeStamp(1);
+
+        $snowflake->setSequenceResolver(function ($t) {
+            global $startTime;
+
+            if (!$startTime) {
+                $startTime = time();
+            }
+
+            // sleep 10 seconds
+            if (time() - $startTime <= 5) {
+                return 256;
+            }
+            return 1;
+        });
+
+        $this->assertNotEmpty($snowflake->id());
+    }
+
+    public function testGetDefaultSequenceResolver()
+    {
+        $snowflake = new IceFlake(1);
+        $this->assertInstanceOf(SequenceResolver::class, $snowflake->getDefaultSequenceResolver());
+        $this->assertInstanceOf(RandomSequenceResolver::class, $snowflake->getDefaultSequenceResolver());
+    }
+
+    public function testGetSequenceResolver()
+    {
+        $snowflake = new IceFlake(9);
+        $this->assertTrue(is_null($snowflake->getSequenceResolver()));
+
+        $snowflake->setSequenceResolver(function () {
+            return 1;
+        });
+
+        $this->assertTrue(is_callable($snowflake->getSequenceResolver()));
+    }
+
+    public function testGetStartTimeStamp()
+    {
+        $snowflake = new IceFlake(111);
+        $defaultTime = '2019-08-08 08:08:08';
+
+        $this->assertTrue($snowflake->getStartTimeStamp() === (strtotime($defaultTime)));
+
+        $snowflake->setStartTimeStamp(1);
+        $this->assertTrue(1 === $snowflake->getStartTimeStamp());
+    }
+
+}

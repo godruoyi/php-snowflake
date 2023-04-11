f71bb5f059db2f1c5f58811bcff610c22109d0b9
Lianbo <lianbo.xu@compass.com>
2023-04-11 12:11:57 +0800

    update pint use custom config

--- src/Snowflake.php
+++ src/Snowflake.php
@@ -123,6 +123,7 @@ class Snowflake
      * Get current millisecond time.
      *
      * @deprecated the method name is wrong, use getCurrentMillisecond instead, will be removed in next major version.
+     *
      * @codeCoverageIgnore
      *
      * @return int
